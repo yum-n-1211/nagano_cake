b503cee6e04dc6eb8e72833cf90550d51731a173
yum-n-1211 <92237680+yum-n-1211@users.noreply.github.com>
2022-02-06 13:14:43 +0000

    [Finish]deviseの導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "c7bfe84831064403ad8daa586d7532d2.vfs.cloud9.us-east-1.amazonaws.com"
+
 end
