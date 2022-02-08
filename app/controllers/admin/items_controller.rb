fdb0820f1ca670413afa813538ecc9676485ccc2
yum-n-1211 <92237680+yum-n-1211@users.noreply.github.com>
2022-02-08 14:08:16 +0000

    [Finish]商品新規登録画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ItemsController < ApplicationController
   def new
+    @item = Item.new
   end
 
   def index
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :genre
-  
-  has_one_attached :image
+
+  attachment :image
 end
