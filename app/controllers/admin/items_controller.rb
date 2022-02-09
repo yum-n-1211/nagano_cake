9e7d396f66e5e94eb21185fe9adf41cd47903706
yum-n-1211 <92237680+yum-n-1211@users.noreply.github.com>
2022-02-09 14:14:28 +0000

    商品一覧

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,8 +2,15 @@ class Admin::ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+  
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
+  end
 
   def index
+    @items = Item.all
   end
 
   def show
@@ -14,6 +21,6 @@ class Admin::ItemsController < ApplicationController
   
   private
   def item_params
-    params.require(:item).permit(:name, :introduction, :price, :is_active, :image)
+    params.require(:item).permit(:name, :introduction, :price, :is_active, :image, :genre_id)
   end
 end
